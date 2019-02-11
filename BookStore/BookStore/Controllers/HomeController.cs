98b1db662af594b4232c119a8ee13d5276343fa1
Крічфолуші Іван <kjanos96@gmail.com>
2019-02-11 18:37:08 +0200

    commit

---
+++ BookStore/BookStore/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using BookStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookStore.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        BookContext db = new BookContext();
+
+        
+
+        public ActionResult Index()
+        {
+            IEnumerable<Book> books = db.Books;
+            ViewBag.Books = books;
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Book book)
+        {
+            db.Entry(book).State = EntityState.Added;
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            Book b = db.Books.Find(id);
+            if (b == null)
+            {
+                return HttpNotFound();
+            }
+            return View(b);
+        }
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Book b = db.Books.Find(id);
+            if (b == null)
+            {
+                return HttpNotFound();
+            }
+            db.Books.Remove(b);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ BookStore/BookStore/Models/BookDBInitializer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using BookStore.Models;
+
+namespace BookStore.Models
+{
+    public class BookDBInitializer : DropCreateDatabaseAlways<BookContext>
+    {
+        protected override void Seed(BookContext db)
+        {
+            db.Books.Add(new Book { BookId = 1, Name = "Harry Potter and the Order of Pheonix", Author = "J. K. Rowling" });
+
+            db.Books.Add(new Book { BookId = 2, Name = "A Dance with Dragons", Author = "George R. R. Martin" });
+
+            base.Seed(db);
+        }
+    }
+}
